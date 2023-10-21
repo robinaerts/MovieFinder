d5189b1dec09511a83c70ba9c2e7334f61eb37e1
Robin Aerts <nibor.aerts@gmail.com>
2023-10-21 23:25:37 +0200

    started to display team members on profile page

--- lib/components/profile.dart
+++ lib/components/profile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,23 +11,92 @@ class Profile extends StatelessWidget {
         .then((_) => {Navigator.of(ctx).pushReplacementNamed("/")});
   }
 
+  Future getTeamMembers(Map<String, dynamic> team) async {
+    final members = team['members'];
+    final memberDataList = <Map<String, dynamic>>[];
+
+    for (final member in members) {
+      try {
+        final memberData = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(member)
+            .get();
+
+        if (memberData.exists) {
+          memberDataList.add(memberData.data() as Map<String, dynamic>);
+        }
+      } catch (e) {
+        // Handle the error, e.g., log it or display an error message
+        print('Error fetching member data: $e');
+      }
+    }
+    return memberDataList;
+  }
+
+  Future getTeamAndMembers() async {
+    var teamDoc = await FirebaseFirestore.instance
+        .collection('groups')
+        .where('members', arrayContains: FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    var team = teamDoc.docs[0].data();
+    var teamMembers = await getTeamMembers(team);
+
+    return {
+      "team": teamDoc.docs[0].data(),
+      "members": teamMembers,
+    };
+  }
+
   final User _user = FirebaseAuth.instance.currentUser!;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        // Image.network(_user!.photoURL.toString()),
-        Text(_user.email.toString()),
-        SizedBox(
-          width: 100,
-          child: OutlinedButton(
-            onPressed: () => logout(context),
-            child: Row(children: const [Icon(Icons.logout), Text("Logout")]),
-          ),
-        )
-      ]),
-    );
+    return FutureBuilder(
+        future: getTeamAndMembers(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Container(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text("Welcome, ${_user.displayName}!"),
+                      Text("Your teamcode is: "),
+                      TextField(
+                        controller: TextEditingController(
+                            text: snapshot.data['team']['code']),
+                        readOnly: true,
+                      ),
+                      Text("Your team members are: "),
+                      ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data['members'].length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              title: Text(snapshot.data['members'][index]
+                                      ['firstname'] +
+                                  " " +
+                                  snapshot.data['members'][index]['lastname']),
+                              subtitle: Text(
+                                  snapshot.data['members'][index]['email']),
+                            );
+                          }),
+                      Text(_user.email.toString()),
+                      SizedBox(
+                        width: 100,
+                        child: OutlinedButton(
+                          onPressed: () => logout(context),
+                          child: Row(children: const [
+                            Icon(Icons.logout),
+                            Text("Logout")
+                          ]),
+                        ),
+                      )
+                    ]));
+          } else {
+            return const CircularProgressIndicator();
+          }
+        });
   }
 }
